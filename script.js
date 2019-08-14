7788b7553b5b0793db22dc06af06da4b01911d06
Brandon Blaschke <brandonj.blaschke@gmail.com>
2019-08-13 19:49:12 -0700

    Icons and script.js added

---
+++ script.js
@@ -0,0 +1,6 @@
+$(function() {
+    $("#content1").hide().fadeIn(2000);
+    $("#content2").hide().fadeIn(3000);
+    $("#content3").hide().fadeIn(4000);
+    $("#content4").hide().fadeIn(5000);
+});
